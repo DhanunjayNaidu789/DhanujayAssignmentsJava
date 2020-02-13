1d4142e6d7da684828242def63121aa53ff87613
Dhanunjay Naidu <dhanusandy789@gmail.com>
2020-02-13 16:01:37 +0530

    added lab10 exercise

---
+++ lab10/exerices1/CopyDataThread.java
@@ -0,0 +1,16 @@
+package com.cg.lab10.exerices1;
+
+
+	import java.io.File;
+
+	public class CopyDataThread 
+	{
+		public File source, target;
+		CopyDataThread(File source,File target)
+		{
+			this.source=source;
+			this.target=target;
+		}
+	}
+
+
